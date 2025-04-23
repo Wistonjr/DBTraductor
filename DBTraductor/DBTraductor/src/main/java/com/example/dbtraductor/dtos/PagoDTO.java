a46ef165da2ac71867eef2b896a17a1d32746c1d
SheylaQuispe <u201620209@upc.edu.pe>
2025-04-23 18:25:01 -0500

    .
    .

---
+++ DBTraductor/DBTraductor/src/main/java/com/example/dbtraductor/dtos/PagoDTO.java
@@ -0,0 +1,52 @@
+package com.example.dbtraductor.dtos;
+
+import java.time.LocalDate;
+
+public class PagoDTO {
+
+    private int idPago;
+    private double montoPago;
+    private String metodoPago;
+    private LocalDate fechaPago;
+    private String estadoPago;
+
+    public int getIdPago() {
+        return idPago;
+    }
+
+    public void setIdPago(int idPago) {
+        this.idPago = idPago;
+    }
+
+    public double getMontoPago() {
+        return montoPago;
+    }
+
+    public void setMontoPago(double montoPago) {
+        this.montoPago = montoPago;
+    }
+
+    public String getMetodoPago() {
+        return metodoPago;
+    }
+
+    public void setMetodoPago(String metodoPago) {
+        this.metodoPago = metodoPago;
+    }
+
+    public LocalDate getFechaPago() {
+        return fechaPago;
+    }
+
+    public void setFechaPago(LocalDate fechaPago) {
+        this.fechaPago = fechaPago;
+    }
+
+    public String getEstadoPago() {
+        return estadoPago;
+    }
+
+    public void setEstadoPago(String estadoPago) {
+        this.estadoPago = estadoPago;
+    }
+}
