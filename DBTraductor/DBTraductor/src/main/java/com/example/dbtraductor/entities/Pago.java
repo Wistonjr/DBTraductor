7228430b72409d745f437df24daebbb0f4c05c82
SheylaQuispe <u201620209@upc.edu.pe>
2025-04-23 13:30:18 -0500

    .
    .

--- DBTraductor/DBTraductor/src/main/java/com/example/dbtraductor/entities/Pago.java
+++ DBTraductor/DBTraductor/src/main/java/com/example/dbtraductor/entities/Pago.java
@@ -1,4 +1,68 @@
 package com.example.dbtraductor.entities;
 
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name="Pago")
 public class Pago {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idPago;
+    @Column (name="montoPago", nullable=false)
+    private double montoPago;
+    @Column (name="metodoPago", nullable=false, length=10)
+    private String metodoPago;
+    @Column (name="fechaPago", nullable=false)
+    private LocalDate fechaPago;
+    @Column (name="estadoPago", nullable=false, length=15)
+    private String estadoPago;
+
+    public Pago(int idPago, double montoPago, String metodoPago, LocalDate fechaPago, String estadoPago) {
+        this.idPago = idPago;
+        this.montoPago = montoPago;
+        this.metodoPago = metodoPago;
+        this.fechaPago = fechaPago;
+        this.estadoPago = estadoPago;
+    }
+
+    public int getIdPago() {
+        return idPago;
+    }
+
+    public void setIdPago(int idPago) {
+        this.idPago = idPago;
+    }
+
+    public double getMontoPago() {
+        return montoPago;
+    }
+
+    public void setMontoPago(double montoPago) {
+        this.montoPago = montoPago;
+    }
+
+    public String getMetodoPago() {
+        return metodoPago;
+    }
+
+    public void setMetodoPago(String metodoPago) {
+        this.metodoPago = metodoPago;
+    }
+
+    public LocalDate getFechaPago() {
+        return fechaPago;
+    }
+
+    public void setFechaPago(LocalDate fechaPago) {
+        this.fechaPago = fechaPago;
+    }
+
+    public String getEstadoPago() {
+        return estadoPago;
+    }
+
+    public void setEstadoPago(String estadoPago) {
+        this.estadoPago = estadoPago;
+    }
 }
--- DBTraductor/DBTraductor/src/main/java/com/example/dbtraductor/entities/Suscripcion.java
+++ DBTraductor/DBTraductor/src/main/java/com/example/dbtraductor/entities/Suscripcion.java
@@ -1,4 +1,79 @@
 package com.example.dbtraductor.entities;
 
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name="Suscripcion")
 public class Suscripcion {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idSuscripcion;
+    @Column (name="nombreSuscripcion", nullable=false, length=20)
+    private String nombreSuscripcion;
+    @Column (name="estadoSuscripion", nullable=false, length=20)
+    private String estadoSuscripcion;
+    @Column (name="fechaInicioSuscripcion", nullable=false)
+    private LocalDate fechaInicioSuscripcion;
+    @Column (name="fechaFinSuscripcion", nullable=false)
+    private LocalDate fechaFinSuscripcion;
+    @Column (name="historialSuscripcion", nullable=false, length=20)
+    private String historialSuscripcion;
+
+    public Suscripcion(int idSuscripcion, String nombreSuscripcion, String estadoSuscripcion, LocalDate fechaInicioSuscripcion, LocalDate fechaFinSuscripcion, String historialSuscripcion) {
+        this.idSuscripcion = idSuscripcion;
+        this.nombreSuscripcion = nombreSuscripcion;
+        this.estadoSuscripcion = estadoSuscripcion;
+        this.fechaInicioSuscripcion = fechaInicioSuscripcion;
+        this.fechaFinSuscripcion = fechaFinSuscripcion;
+        this.historialSuscripcion = historialSuscripcion;
+    }
+
+    public int getIdSuscripcion() {
+        return idSuscripcion;
+    }
+
+    public void setIdSuscripcion(int idSuscripcion) {
+        this.idSuscripcion = idSuscripcion;
+    }
+
+    public String getNombreSuscripcion() {
+        return nombreSuscripcion;
+    }
+
+    public void setNombreSuscripcion(String nombreSuscripcion) {
+        this.nombreSuscripcion = nombreSuscripcion;
+    }
+
+    public String getEstadoSuscripcion() {
+        return estadoSuscripcion;
+    }
+
+    public void setEstadoSuscripcion(String estadoSuscripcion) {
+        this.estadoSuscripcion = estadoSuscripcion;
+    }
+
+    public LocalDate getFechaInicioSuscripcion() {
+        return fechaInicioSuscripcion;
+    }
+
+    public void setFechaInicioSuscripcion(LocalDate fechaInicioSuscripcion) {
+        this.fechaInicioSuscripcion = fechaInicioSuscripcion;
+    }
+
+    public LocalDate getFechaFinSuscripcion() {
+        return fechaFinSuscripcion;
+    }
+
+    public void setFechaFinSuscripcion(LocalDate fechaFinSuscripcion) {
+        this.fechaFinSuscripcion = fechaFinSuscripcion;
+    }
+
+    public String getHistorialSuscripcion() {
+        return historialSuscripcion;
+    }
+
+    public void setHistorialSuscripcion(String historialSuscripcion) {
+        this.historialSuscripcion = historialSuscripcion;
+    }
 }
