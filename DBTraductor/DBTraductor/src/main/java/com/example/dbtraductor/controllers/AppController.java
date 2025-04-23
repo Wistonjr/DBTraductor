fdb52f84f24e428a2cdff995faf74a2e7141439a
Wistonjr <u201719417@upc.edu.pe>
2025-04-23 03:00:02 -0500

    agregue un comentario en el AppController

--- DBTraductor/DBTraductor/src/main/java/com/example/dbtraductor/controllers/AppController.java
+++ DBTraductor/DBTraductor/src/main/java/com/example/dbtraductor/controllers/AppController.java
@@ -14,6 +14,7 @@ import java.util.stream.Collectors;
 @RestController
 @RequestMapping("/usuarios")
 public class AppController {
+    //comentario prueba
     @Autowired
     private IAppService aS;
 
