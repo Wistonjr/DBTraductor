fca3343a40dd207263adc362d115475666830f12
Wistonjr <u201719417@upc.edu.pe>
2025-04-29 20:02:33 -0500

    Agregue 2 querys

--- DBTraductor/DBTraductor/src/main/java/com/example/dbtraductor/controllers/RolController.java
+++ DBTraductor/DBTraductor/src/main/java/com/example/dbtraductor/controllers/RolController.java
@@ -32,7 +32,7 @@ public class RolController {
         Rol l= m.map(dto, Rol.class);
         rS.insert(l);
     }
-//prueba22
+//subido al main
     @GetMapping("/usuarios")
     public List<RolDto> ListarCantidadModeradores(){
         List<String[]> filaLista=rS.QuantityModeradoresByApp();
