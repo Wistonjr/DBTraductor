722ff2131304c5380509c123cb31f21332f35e09
Wistonjr <u201719417@upc.edu.pe>
2025-05-11 08:01:55 -0500

    Agregue seguridad

---
+++ DBTraductor/DBTraductor/src/main/java/com/example/dbtraductor/Securities/JwtResponse.java
@@ -0,0 +1,17 @@
+package com.example.dbtraductor.Securities;
+
+public class JwtResponse {
+
+    private static final long serialVersionUID = -8091879091924046844L;
+    private final String jwttoken;
+
+    public String getJwttoken() {
+        return jwttoken;
+    }
+
+    public JwtResponse(String jwttoken) {
+        super();
+        this.jwttoken = jwttoken;
+    }
+
+}
